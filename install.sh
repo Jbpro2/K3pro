7f2e1bd28c97e81b962dec4735174acced6ddb00
MTSPRO2 <mtspro444@gmail.com>
2026-07-24 23:57:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SDProxy Installer - Version v3.1.0
 
-REPO_URL="https://github.com/PedroJbk/BDRProxy.git"
+REPO_URL="https://github.com/PedroJbk/CTProxy.git"
 REPO_BRANCH="main"
 CMD_NAME="sdproxy"
 TOTAL_STEPS=7
