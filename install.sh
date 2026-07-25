3115b1bb6d8110024f41462bb6177a995816ce56
Lacasx1 <silvawoo12@gmail.com>
2026-07-25 09:41:48 -0300

    Update repository URL in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SDProxy Installer - Version v3.1.0
 
-REPO_URL="https://github.com/MTSPRO2/CTProxy.git"
+REPO_URL="https://github.com/Lacasx1/LKProxy.git"
 REPO_BRANCH="main"
 CMD_NAME="sdproxy"
 TOTAL_STEPS=7
