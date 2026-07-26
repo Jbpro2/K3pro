a2f563aecaf141decdc6d01a4bece4dc4831ab01
Lacasx1 <silvawoo12@gmail.com>
2026-07-26 01:20:52 -0300

    Update repository URL in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LKProxy Installer - Version v3.1.0
 
-REPO_URL="https://github.com/Lacasx1/LKProxy.git"
+REPO_URL="https://github.com/Lacasx1/Xtpro.git"
 REPO_BRANCH="main"
 CMD_NAME="lkproxy"
 TOTAL_STEPS=7
